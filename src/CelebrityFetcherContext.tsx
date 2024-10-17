657bf14e7d1a0c32b1b1cdd3c50547dbb2bd02a3
Jason Stine <jason102@gmail.com>
2024-10-16 22:56:33 -0700

    Fetch popular celebrities on app load

--- src/CelebrityFetcherContext.tsx
+++ src/CelebrityFetcherContext.tsx
@@ -1,18 +1,25 @@
-import { createContext, useState, Dispatch, SetStateAction } from "react";
-
-type Celebrity = {
-  name: string;
-};
+import {
+  createContext,
+  useState,
+  Dispatch,
+  SetStateAction,
+  useEffect,
+} from "react";
+import { TheMoveDBApiResponse, Celebrity } from "./types";
 
 interface CelebrityFetcherContextType {
   celebrities: Celebrity[];
   setCelebrities: Dispatch<SetStateAction<Celebrity[]>>;
+  loading: boolean;
+  error: string;
 }
 
 export const CelebrityFetcherContext =
   createContext<CelebrityFetcherContextType>({
     celebrities: [],
     setCelebrities: () => {},
+    loading: true,
+    error: "",
   });
 
 interface Props {
@@ -21,9 +28,43 @@ interface Props {
 
 const CelebrityFetcherProvider: React.FC<Props> = ({ children }) => {
   const [celebrities, setCelebrities] = useState<Celebrity[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  const fetchCelebrities = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/person/popular?api_key=${
+          import.meta.env.VITE_THE_MOVIE_DB_API_KEY
+        }`
+      );
+
+      if (!response.ok) {
+        throw new Error(`Ooops, something went wrong: ${response.statusText}`);
+      }
+
+      const apiData: TheMoveDBApiResponse = await response.json();
+
+      console.log(apiData);
+    } catch (err) {
+      console.log({ err });
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Load some celebrities when the app first loads
+  useEffect(() => {
+    fetchCelebrities();
+  }, []);
 
   return (
-    <CelebrityFetcherContext.Provider value={{ celebrities, setCelebrities }}>
+    <CelebrityFetcherContext.Provider
+      value={{ celebrities, setCelebrities, loading, error }}
+    >
       {children}
     </CelebrityFetcherContext.Provider>
   );
---
+++ src/types.ts
@@ -0,0 +1,38 @@
+interface KnownFor {
+  backdrop_path: string | null;
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string | null;
+  media_type: string;
+  adult: boolean;
+  original_language: string;
+  genre_ids: number[];
+  popularity: number;
+  release_date: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
+
+interface TheMoveDBApiResponseCelebrity {
+  adult: boolean;
+  gender: number;
+  id: number;
+  known_for_department: string;
+  name: string;
+  original_name: string;
+  popularity: number;
+  profile_path: string | null;
+  known_for: KnownFor[];
+}
+
+export interface TheMoveDBApiResponse {
+  page: number;
+  results: TheMoveDBApiResponseCelebrity[];
+}
+
+export interface Celebrity {
+  name: string;
+}
